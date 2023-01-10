79cbfb6d27908395a5389664f9562031b9ac90fb
Kevin J. Sung <kevjsung@umich.edu>
2023-01-10 09:31:02 -0500

    remove some mod taps

--- keyboards/handwired/dactyl_promicro/keymaps/kevinsung/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/kevinsung/keymap.c
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV        , KC_1          , KC_2          , KC_3   , KC_4   , KC_5   ,                    KC_6   , KC_7  , KC_8   , KC_9   ,  KC_0  , KC_MINS,
         KC_TAB        , KC_Q          , KC_W          , KC_E   , KC_R   , KC_T   ,                    KC_Y   , KC_U  , KC_I   , KC_O   ,  KC_P  , KC_EQL ,
         C_S_T(KC_CAPS), KC_A          , KC_S          , KC_D   , KC_F   , KC_G   ,                    KC_H   , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-        RALT(KC_RWIN) , KC_Z          , KC_X          , KC_C   , KC_V   , KC_B   ,                    KC_N   , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
-        LSFT_T(KC_F10), LCTL_T(KC_F11), RALT_T(KC_F12), KC_LEFT, KC_RGHT, _______,                    _______, KC_UP , KC_DOWN, KC_LBRC, KC_RBRC, RCTL_T(KC_F8),
-                                                    KC_LWIN, LSFT_T(KC_F4), KC_F5,                    KC_DEL, KC_ENT, KC_RALT,
+        LALT(KC_RWIN) , KC_Z          , KC_X          , KC_C   , KC_V   , KC_B   ,                    KC_N   , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
+        KC_F12        , KC_LCTL       , KC_LALT       , KC_LEFT, KC_RGHT, _______,                    _______, KC_UP , KC_DOWN, KC_LBRC, KC_RBRC, RCTL_T(KC_F8),
+                                                          KC_LWIN, KC_LSFT, KC_F5,                    KC_DEL, KC_ENT, KC_RALT,
                                                                         TG(RAISE),                    TG(NUM),
                                                                            KC_SPC,                    KC_BSPC,
                                                                         MO(LOWER),                    KC_RSFT
