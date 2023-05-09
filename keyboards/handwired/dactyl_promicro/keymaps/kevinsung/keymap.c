0218a8e80a2c86a14b4ccb054e7a05ef76a7bcfd
Kevin J. Sung <kevjsung@umich.edu>
2023-05-09 11:56:06 -0400

    f11

--- keyboards/handwired/dactyl_promicro/keymaps/kevinsung/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/kevinsung/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         C_S_T(KC_CAPS), KC_A          , KC_S          , KC_D   , KC_F   , KC_G   ,                    KC_H   , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
         LALT(KC_RWIN) , KC_Z          , KC_X          , KC_C   , KC_V   , KC_B   ,                    KC_N   , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
         KC_F12        , KC_LCTL       , KC_LALT       , KC_LEFT, KC_RGHT, _______,                    _______, KC_UP , KC_DOWN, KC_LBRC, KC_RBRC, RCTL_T(KC_F8),
-                                                          KC_LWIN, KC_LSFT, KC_F5,                    KC_DEL, KC_ENT, KC_RALT,
+                                                          KC_LWIN, KC_LSFT, KC_F11,                    KC_DEL, KC_ENT, KC_RALT,
                                                                         TG(RAISE),                    TG(NUM),
                                                                            KC_SPC,                    KC_BSPC,
                                                                         MO(LOWER),                    KC_RSFT
